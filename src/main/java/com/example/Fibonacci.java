93e71685d9dad498b30c1dc8a120b3ce8699aefb
rptl8sr <rptl8sr@gmail.com>
2025-07-04 01:11:12 +0300

    add Recursive Fibonacci Implementation

---
+++ src/main/java/com/example/Fibonacci.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import lombok.extern.java.Log;
+
+@Log
+public class Fibonacci {
+    public static void main(String... args) {
+        System.out.println("FibonacciRecursive " + fibonacciRecursive(10));
+    }
+
+    public static long fibonacciRecursive(int n) {
+        if (n < 0) {
+            log.severe("n must be greater than or equal to 0");
+            throw new IllegalArgumentException("n must be greater than or equal to 0");
+        }
+
+        if (n <= 1) {
+            return n;
+        }
+
+        long res = fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+        log.fine("res = " + res + ", n = " + n);
+        return res;
+    }
+}
