5ce136bf917b4a660d933f490b0607c1a8749b0b
rptl8sr <rptl8sr@gmail.com>
2025-07-04 12:30:35 +0300

    add Iterative algorithm

--- src/main/java/com/example/Fibonacci.java
+++ src/main/java/com/example/Fibonacci.java
@@ -24,16 +24,12 @@ public class Fibonacci {
         Fibonacci fibonacci = new Fibonacci();
         System.out.println("Fibonacci Recursive " + fibonacci.Recursive(10));
         System.out.println("Fibonacci Memoized " + fibonacci.Memoized(10));
+        System.out.println("Fibonacci Iterative " + fibonacci.Iterative(10));
     }
 
     public long Recursive(int n) {
-        if (n < 0) {
-            log.severe("n must be greater than or equal to 0");
-            throw new IllegalArgumentException("n must be greater than or equal to 0");
-        }
-
-        if (n <= 1) {
-            return n;
+        if (n < 2) {
+            return validateNumberCondition(n);
         }
 
         long res = Recursive(n-1) + Recursive(n-2);
@@ -42,13 +38,8 @@ public class Fibonacci {
     }
 
     public long Memoized(int n) {
-        if (n < 0) {
-            log.severe("n must be greater than or equal to 0");
-            throw new IllegalArgumentException("n must be greater than or equal to 0");
-        }
-
-        if (n <= 1) {
-            return n;
+        if (n < 2) {
+            return validateNumberCondition(n);
         }
 
         if (cache.containsKey(n)) {
@@ -59,5 +50,36 @@ public class Fibonacci {
         log.fine("res = " + res + ", n = " + n);
         cache.put(n, res);
         return res;
+    }
+
+    public long Iterative(int n) {
+        if (n < 2) {
+            return validateNumberCondition(n);
+        }
+
+        long prevNumber = 1;
+        long prevPrevNumber = 0;
+        long current = 0;
+
+        for (int i = 1; i < n; i++) {
+            current = prevNumber + prevPrevNumber;
+            prevPrevNumber = prevNumber;
+            prevNumber = current;
+            log.fine("current = " + current + ", prevNumber = " + prevNumber + ", prevPrevNumber = " + prevPrevNumber);
+        }
+
+        return current;
+    }
+
+    private long validateNumberCondition(int n) {
+        if (n < 0) {
+            log.severe("n must be greater than or equal to 0");
+            throw new IllegalArgumentException("n must be greater than or equal to 0");
+        }
+        if (n <= 1) {
+            return n;
+        }
+
+        return -1;
     }
 }
