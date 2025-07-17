27ff9c6e6466531b6b031d8988aee360357d3abb
rptl8sr <rptl8sr@gmail.com>
2025-07-17 15:57:12 +0300

    init custom runner class

---
+++ src/test/java/CustomTestRunner.java
@@ -0,0 +1,10 @@
+public class CustomTestRunner {
+    static int total = 0;
+    static int passed = 0;
+    static int failed = 0;
+    static long time = 0;
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
